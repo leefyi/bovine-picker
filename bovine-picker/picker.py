adec51c42d0b2fae0467549aece34dab7b09499b
李芳毅 <leefyi@126.com>
2019-02-18 21:56:33 +0800

    commit picker project

---
+++ bovine-picker/picker.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin python
+# coding=utf-8
+# @Time    : 2019-02-18 21:54
+# @Author  : lifangyi
+# @File    : picker.py
+# @Software: PyCharm
+
+
+class Picker(object):
+
+    def __init__(self):
+        self.description = 'check out the day'
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin python
+# coding=utf-8
+# @Time    : 2019-02-18 21:42
+# @Author  : lifangyi
+# @File    : setup.py
+# @Software: PyCharm
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'bovine-picker',
+    version = '0.0.1',
+    keywords='bovine-birthday test pypi',
+    description = 'a library for Test pypi distribution',
+    license = 'MIT License',
+    url = 'https://github.com/leefyi/bovine-picker',
+    author = 'lifangyi',
+    author_email = 'leefyi@126.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
