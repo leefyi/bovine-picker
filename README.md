c3041a5ce73a0dd2c18e449cacc4b33715d4ccba
李芳毅 <leefyi@126.com>
2019-02-18 23:20:23 +0800

    complete the picker function

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
+bovine-picker
 
 1.a test lib/project of pypi distribution process.
+
 2.a simple usage of Bovine Birthday formula. 
+
 3.check out what day of a week with birthday as parameter in three languages:Simplified Chinese, English, Japanese.
-4.Only date after AD 1582.10.15 is verified and available.
+
+4.Only date after AD 1582.10.15 is verified and valid.
+
+5.MIT License.
+
--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'bovine-picker',
-    version = '0.0.1',
+    name='bovine-picker',
+    version='0.0.1',
     keywords='bovine-birthday test pypi',
-    description = 'a library for Test pypi distribution',
-    license = 'MIT License',
-    url = 'https://github.com/leefyi/bovine-picker',
-    author = 'lifangyi',
-    author_email = 'leefyi@126.com',
-    packages = find_packages(),
-    include_package_data = True,
-    platforms = 'any',
-    install_requires = [],
+    description='a library for test pypi distribution process',
+    license='MIT License',
+    url='https://github.com/leefyi/bovine-picker',
+    author='lifangyi',
+    author_email='leefyi@126.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
 )
